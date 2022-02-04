f51ccec5dc1859f29b757a93d8b516d7d902d1ff
s4ncheese <alex_rusanov03@mail.ru>
2022-02-04 20:58:43 +0300

    try 1000000000

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-#include "libs\data_structures\unordered_array_set\unordered_array_set.h"
-#include "libs\data_structures\bitset\bitset.h"
-#include "libs\algorithms\array\array.h"
 #include "libs\data_structures\vectors\vector.h"
 #include <stdint.h>
 
