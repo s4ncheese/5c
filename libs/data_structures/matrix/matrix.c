b3aa78c4f5217d212cdf62a40e80cba895e8e888
s4ncheese <alex_rusanov03@mail.ru>
2022-02-11 21:11:40 +0300

    memory usage edit

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -4,7 +4,7 @@
 // Возвращает матрицу размером nRows на nCols, размещенную в
 // динамической памяти
 matrix getMemMatrix(int nRows, int nCols) {
-    int **values = (int**) malloc(sizeof(int*) * nRows);
+    int **values = (int **) malloc(sizeof(int *) * nRows);
     for (int i = 0; i < nRows; i++)
         values[i] = malloc(sizeof(int) * nCols);
 
@@ -14,7 +14,7 @@ matrix getMemMatrix(int nRows, int nCols) {
 // Возвращает массив из nMatrices матриц размером nRows на nCols,
 // размещенный в динамической памяти
 matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols) {
-    matrix *matrixArray = (matrix*) malloc(sizeof(matrix) * nMatrices);
+    matrix *matrixArray = (matrix *) malloc(sizeof(matrix) * nMatrices);
     for (int matrixIndex = 0; matrixIndex < nMatrices; matrixIndex++)
         matrixArray[matrixIndex] = getMemMatrix(nRows, nCols);
 
@@ -32,5 +32,5 @@ void freeMemMatrix(matrix m) {
 // nMatrices матриц
 void freeMemMatrices(matrix *mArray, int nMatrices) {
     for (int mNumber = 0; mNumber < nMatrices; mNumber++)
-        freeMemMatrix((matrix)mArray[mNumber]);
+        freeMemMatrix((matrix) mArray[mNumber]);
 }
